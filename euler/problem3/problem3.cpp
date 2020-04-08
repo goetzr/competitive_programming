c572a4c44ef672a44f084d45e7f5d63cbe9b9794
Russ Goetz <russgoetz@gmail.com>
2020-04-07 20:43:38 -0400

    Euler problem 3: kipping even numbers when finding primes, cleaned up and commented code.

--- euler/problem3/problem3.cpp
+++ euler/problem3/problem3.cpp
@@ -1,50 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
 #include <cmath>
 
 using namespace std;
 
-template <typename T>
-bool contains(vector<T> vec, T val) {
-  return find(begin(vec), end(vec), val) != end(vec);
-}
-
-template <typename T>
-void print(vector<T> vec) {
-  for (int i = 0; i < vec.size(); ++i) {
-    cout << vec[i];
-    if (i < vec.size() - 1) {
-      cout << ", ";
-    }
-  }
-}
-
-/*
-vector<uint64_t> factors(uint64_t n) {
-  vector<uint64_t> result;
-
-  result.push_back(n);
-  for (auto i = n / 2; i >= 2; --i) {
-    if (n % i == 0) {
-      result.push_back(i);
-    }  
-  }
-  result.push_back(1);
-
-  return result;
-}
-*/
-
+/**
+ * Find the prime numbers up to and including the specified number.
+ * The algorithm used is based on the Sieve of Eratosthenes.
+ * Instead of generating a list of consecutive integers in which non-primes are marked,
+ * which uses too much memory for large n, each number if checked against a list of
+ * primes already found. If it's evenly divisible by any of the primes arelady found,
+ * it's not a prime.
+ * 
+ * @param n   Find primes up to (and including) this number.
+ * 
+ * @return A list of the primes up to (and including) the specified number.
+ **/ 
 vector<uint64_t> primes(uint64_t n) {
   std::vector<uint64_t> result;
   if (n < 2) {
     return result;
   }
+  // 2 is the only even number that's prime.
   result.push_back(2);
 
-  for (uint64_t i = 3; i <= n; ++i) {
+  // i += 2 ==> Skip over all other even numbers, which are never prime.
+  for (uint64_t i = 3; i <= n; i += 2) {
+    // Assume the current number is prime.
+    // If it's evenly divisible by any of the primes already found, then it's not a prime.
     bool is_prime = true;
     for (auto prime : result) {
       if (i % prime == 0) {
@@ -62,12 +46,23 @@ vector<uint64_t> primes(uint64_t n) {
 
 class Problem3 {
  public:
+ /**
+  * Find the largest prime factor (lpf) of the specified number.
+  * 
+  * @param n    The number whose largest prime factor should be found.
+  * 
+  * @return The largest prime factor of the specified number.
+  **/
   uint64_t lpf(uint64_t n) {
     uint64_t result = 0;
 
+    // Only need to generate primes up to sqrt(n), as factors of n can't be larger than sqrt(n).
+    // This is dramatically faster when n is large.
     auto pm = primes((uint64_t)sqrt(n));
-    sort(begin(pm), end(pm), [](auto a, auto b) { return a > b; });
 
+    // Sort the primes from largest to smallest, then test them in order.
+    // The first factor of n found is its largest prime factor.
+    sort(begin(pm), end(pm), [](auto a, auto b) { return a > b; });
     for (auto prime : pm) {
       if (n % prime == 0) {
         result = prime;
