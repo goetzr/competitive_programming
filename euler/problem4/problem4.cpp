c03f3dc9deeb29b6bad6015367f6992fbfff737a
Russ Goetz <russgoetz@gmail.com>
2020-04-13 19:49:34 -0400

    Euler problem 4: added comments to the solution.

--- euler/problem4/problem4.cpp
+++ euler/problem4/problem4.cpp
@@ -4,11 +4,13 @@
 
 using namespace std;
 
-#define ASSERT(condition)                           \
-if (!(condition)) {                                 \
-  cerr << "ASSERT FAILED: " << #condition << endl;  \
-}
-
+/**
+ * Computes the number of digits in the specified number
+ * 
+ * @param n The number whose number of digits should be found.
+ * 
+ * @return The number of digits in the specified number.
+ */
 int num_digits(int n) {
   // Ignore the special case where n = 0.
   int nd = 0;
@@ -19,6 +21,13 @@ int num_digits(int n) {
   return nd;
 }
 
+/**
+ * Calculates 10 to the specified power.
+ * 
+ * @param n The power to raise 10 to.
+ * 
+ * @return 10 ^ n.
+ */
 int pow10(int n) {
   int result = 1;
   for (int i = 0; i < n; ++i) {
@@ -27,15 +36,37 @@ int pow10(int n) {
   return result;
 }
 
+/**
+ * Returns the desired number of digits from the right side of the specified number.
+ * 
+ * @param n       The number whose right-side digits should be returned.
+ * @param ndigits The number of right-side digits to return.
+ * 
+ * @return The ndigits digits from the specified number.
+ */
 int right_digits(int n, int ndigits) {
   auto scale = pow10(ndigits);
   return n - (n / scale * scale);
 }
 
+/**
+ * Returns the right-most digit of the specified number.
+ * 
+ * @param n The number whose right-most digits should be returned.
+ * 
+ * @return The right-most digit of the specified number.
+ */
 int right_digit(int n) {
   return right_digits(n, 1);
 }
 
+/**
+ * Returns the digits of the specified number reversed.
+ * 
+ * @param n The number whose digits should be returned in a reverse order.
+ * 
+ * @return The digits of the specified number in reverse order.
+ */
 int reversed_digits(int n) {
   int reversed = 0;
   while (n > 0) {
@@ -45,8 +76,6 @@ int reversed_digits(int n) {
   }
   return reversed;
 }
-
-class NotPalindromeException {};
 
 /**
  * Represents a Palindromic number.
@@ -78,6 +107,8 @@ class Palindrome {
   /**
    * Represents the specified palindromic number. If the number is not a palindrome,
    * the nearest palindrome to the number is represented.
+   * 
+   * @param n The number to represent.
    */
   Palindrome(int n) {
     nd_ = num_digits(n);
@@ -88,10 +119,18 @@ class Palindrome {
     mirror_ = get_mirror();
   }
 
+  /**
+   * Returns the value of the palindrome.
+   *
+   * @return The value of the palindrome.
+   */
   int value() const {
     return core_ * pow10(nd_ / 2) + reversed_digits(mirror_);
   }
 
+  /**
+   * Increments the palindrome to the next largest palindrome.
+   */
   void increment() {
     auto nd_last_core = num_digits(core_);
     ++core_;
@@ -102,6 +141,9 @@ class Palindrome {
     mirror_ = get_mirror();
   }
 
+  /**
+   * Decrements the palindrome to the next smallest palindrome.
+   */
   void decrement() {
     auto nd_last_core = num_digits(core_);
     --core_;
@@ -112,19 +154,41 @@ class Palindrome {
   }
 
  private:
+  /**
+   * Gets the mirror from the core and the number of digits.
+   */
   int get_mirror() {
     // When forming the mirror, remove the number's middle digit from the right side of the core
     // for numbers with an odd number of digits.
     return (nd_ % 2 == 0) ? core_ : core_ / 10;
   }
 
+  /**
+   * The number of digits in the palindrome.
+   */
   int nd_;
+
+  /**
+   * The palindrome's core.
+   */
   int core_;
+
+  /**
+   * The palindrome's mirror.
+   */
   int mirror_; 
 };
 
 class Problem4 {
  public:
+  /**
+   * Computes the largest palindrome that's a product of two numbers having the specified number of digits.
+   * 
+   * @param ndigits   The number of digits in each number.
+   * @param factors   [out] Returns the two factors whose product is equal to the palindrome returned.
+   * 
+   * @return The largest palindrome that's a product of two numbers having the specified number of digits.
+   */
   int lpp(int ndigits, pair<int, int>& factors) {
     auto sp = find_starting_palindrome(ndigits);
     while (!has_factor_pair_with_ndigits(sp.value(), ndigits, factors)) {
@@ -134,6 +198,14 @@ class Problem4 {
   }
 
  private:
+  /**
+   * Finds the starting palindrome. This is the largest palindrome that's not greater than the largest
+   * product formed by multiplying two numbers with the specified number of digits.
+   * 
+   * @param ndigits The number of digits in the two numbers that are being multiplied.
+   * 
+   * @return The starting palindrome.
+   */
   Palindrome find_starting_palindrome(int ndigits) {
     // Find the largest number with the specified number of digits.
     auto n = pow10(ndigits) - 1;
@@ -150,9 +222,22 @@ class Problem4 {
     return starting_palindrome;
   }
 
+  /**
+   * Returns whether specified palindrome has two factors with the specified number of digits.
+   * 
+   * @param n         The palindrome being tested.
+   * @param ndigits   The number of digits in the two factors.
+   * @param factors   [out] If found, returns the two factors used to form the palindrome.
+   * 
+   * @return Returns whether the specified palindrome has two factors with the specified number of digits.
+   */
   bool has_factor_pair_with_ndigits(int n, int ndigits, pair<int, int>& factors) {
+    // Start looking for factors from the smallest number with the specified number of digits.
     auto starting_factor = pow10(ndigits - 1); 
+    // Only look for factors having the specified number of digits up to the square root of the number.
     for (auto i = starting_factor; num_digits(i) == ndigits && i < sqrt(n); ++i) {
+      // If the current number is a factor, and its associated factor also has the specified number of digits,
+      // return true.
       if (n % i == 0 && num_digits(n / i) == ndigits) {
         factors.first = i;
         factors.second = n / i;
