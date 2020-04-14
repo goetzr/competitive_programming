8be87d2094d67c16f82327c542a082d1bad3bf1f
Russ Goetz <russgoetz@gmail.com>
2020-04-13 20:35:13 -0400

    Euler problem 5: initial structure.

---
+++ euler/problem5/problem5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Problem5 {
+ public:
+  /**
+   * Smallest Possible Number Evenly Divisible By (SPNED).
+   */
+  int spnedb(int range_upper) {
+    return 0;
+  }
+};
+
+int main() {
+  Problem5 p5;
+
+  cout << "Smallest possible number evenly divisible by 1 through " << 10 << ": " << p5.spnedb(10) << endl;
+
+  return 0;
+}
